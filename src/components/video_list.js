4484b883cbb041424bb15a59d37627fe597d3ad2
Adam Morsi <adam.morsi@yahoo.com>
2021-09-06 19:12:01 +0300

    format video_list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,18 +1,18 @@
 import React from "react";
 import VideoListItem from "./video_list_item";
 
-const VideoList = props => {
-  const videoItems = props.videos.map(video => {
-    return (
-      <VideoListItem
-        onVideoSelect={props.onVideoSelect}
-        key={video.etag}
-        video={video}
-      />
-    );
-  });
+const VideoList = ({videos, onVideoSelect}) => {
+    const videoItems = videos.map(video => {
+        return (
+            <VideoListItem
+                onVideoSelect={onVideoSelect}
+                key={video.etag}
+                video={video}
+            />
+        );
+    });
 
-  return <ul className="col-md-4 list-group">{videoItems}</ul>;
+    return <ul className="col-md-4 list-group">{videoItems}</ul>;
 };
 
 export default VideoList;
